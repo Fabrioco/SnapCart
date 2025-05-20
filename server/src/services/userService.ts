a591a01dd376f5755cfcdb363186531cdadbd3c4
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-20 19:21:19 -0300

    feat(users): criar função de criar usuário

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,11 +1,23 @@
 import prisma from "../prismaClient/prismaClient";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = () => {
   return prisma.user.findMany();
 };
 
-export const createUser = (name: string, email: string, password: string) => {
+export const createUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const findUser = await prisma.user.findMany({ where: { email } });
+  if (findUser) {
+    throw new Error("Email já cadastrado");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  
   return prisma.user.create({
-    data: { name, email, password },
+    data: { name, email, password: hashPassword },
   });
 };
