9aaa02c82447522809d2ef368858da2691791042
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-21 15:42:22 -0300

    feat(userRoute): adicionei o controller na rota de atualizar os dados do usuário

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,16 @@
 import express from "express";
-import { getUsers, addUser, getUser } from "../controllers/userController";
+import {
+  getUsers,
+  addUser,
+  getUser,
+  updateUser,
+} from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", addUser);
 router.get("/users/:id", getUser);
+router.patch("/users/:id", updateUser);
 
 export default router;
