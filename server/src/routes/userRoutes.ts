8f174ea70fc0293755d8d21425e346c6bd3c2885
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-21 00:16:36 -0300

    feat: criei a rota que usa o controller getUser

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers, addUser } from "../controllers/userController";
+import { getUsers, addUser, getUser } from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", addUser);
+router.get("/users/:id", getUser);
 
 export default router;
