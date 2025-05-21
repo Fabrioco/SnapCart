f4b186d8dd10ecd7d97b3e3b3a5a38dbfb2fbfce
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-21 00:03:26 -0300

    feat: criei o controller do service getOneUser

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,9 +1,30 @@
 import { Request, Response } from "express";
-import { createUser, getAllUsers } from "../services/userService";
+import { createUser, getAllUsers, getOneUser } from "../services/userService";
 
 export const getUsers = async (req: Request, res: Response) => {
   const users = await getAllUsers();
   res.json(users);
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  const id = req.params;
+  if (!id) {
+    res.status(404).json({ error: "Id do usuário não encontrado" });
+  }
+
+  try {
+    return await getOneUser(Number(id));
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.message === "Usuário não encontrado") {
+        res.status(404).json(error.message);
+        return;
+      }
+      res.status(400).json(error.message);
+      return;
+    }
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
 };
 
 export const addUser = async (req: Request, res: Response): Promise<void> => {
