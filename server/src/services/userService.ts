48363d491af181f80a63b84cee1e04e09bd30095
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-20 23:27:10 -0300

    feat: criei o service addUser

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -10,14 +10,22 @@ export const createUser = async (
   email: string,
   password: string
 ) => {
-  const findUser = await prisma.user.findMany({ where: { email } });
-  if (findUser) {
-    throw new Error("Email já cadastrado");
-  }
+  try {
+    const findUser = await prisma.user.findUnique({ where: { email } });
+    if (findUser) {
+      throw new Error("Email já cadastrado");
+    }
 
-  const hashPassword = await bcrypt.hash(password, 10);
-  
-  return prisma.user.create({
-    data: { name, email, password: hashPassword },
-  });
+    const hashPassword = await bcrypt.hash(password, 10);
+    password = hashPassword;
+
+    return prisma.user.create({
+      data: { name, email, password },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+    throw new Error("Erro desconhecido ao registrar");
+  }
 };
