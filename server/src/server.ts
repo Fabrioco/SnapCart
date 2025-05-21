589831f0877d63e0ecd8aa074d4c72895b0870f8
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-21 16:16:42 -0300

    feat(server): adicionei o uso do cookieParser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser'
 
 dotenv.config();
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
 const PORT = process.env.PORT;
 
 app.use("/api", userRoutes);
