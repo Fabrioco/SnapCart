eb06b30b3a13062931c3d7ca5a8e704acc5af6da
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-20 23:41:57 -0300

    feat: adicionei jwt pra criar o token com o email

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,5 +1,6 @@
 import prisma from "../prismaClient/prismaClient";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const getAllUsers = () => {
   return prisma.user.findMany();
@@ -19,9 +20,21 @@ export const createUser = async (
     const hashPassword = await bcrypt.hash(password, 10);
     password = hashPassword;
 
-    return prisma.user.create({
+    const token = jwt.sign({ email }, "secret", { expiresIn: "7d" });
+    if (!token) {
+      throw new Error("Erro ao criar token");
+    }
+
+    const userCreated = await prisma.user.create({
       data: { name, email, password },
     });
+
+    const { password: pass, ...userWithoutPassword } = userCreated;
+
+    return {
+      access_token: token,
+      userWithoutPassword,
+    };
   } catch (error: unknown) {
     if (error instanceof Error) {
       throw new Error(error.message);
