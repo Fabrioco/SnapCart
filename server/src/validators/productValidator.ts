3d8cc1b19ea609a06c860f708bfbc87a38973283
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-22 13:10:01 -0300

    feat(productValidator): adicionei o category e image

--- server/src/validators/productValidator.ts
+++ server/src/validators/productValidator.ts
@@ -3,8 +3,10 @@ import { z } from "zod";
 export const createProductSchema = z.object({
   name: z.string().min(2, "Nome muito curto"),
   description: z.string().min(2, "Descrição muito curta"),
+  category: z.string().min(2, "Categoria inválida"),
   price: z.number().min(1, "Preço inválido"),
   quantity: z.number().min(1, "Quantidade inválida"),
+  image: z.string().min(2, "Link da imagem inválido"),
 });
 
 export type CreateProductInput = z.TypeOf<typeof createProductSchema>;
