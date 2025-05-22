1d1b4986e3c18c833047bc6393a9488318288eb2
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-22 14:31:29 -0300

    feat(productValidator): criei o updateProductInput para verificação

--- server/src/validators/productValidator.ts
+++ server/src/validators/productValidator.ts
@@ -8,4 +8,13 @@ export const createProductSchema = z.object({
   image: z.string().min(2, "Link da imagem inválido"),
 });
 
+export const updateProductSchema = z.object({
+  name: z.string().min(2, "Nome muito curto").optional(),
+  description: z.string().min(2, "Descrição muito curta").optional(),
+  category: z.string().min(2, "Categoria inválida").optional(),
+  price: z.number().min(1, "Preço inválido").optional(),
+  image: z.string().min(2, "Link da imagem inválido").optional(),
+});
+
 export type CreateProductInput = z.TypeOf<typeof createProductSchema>;
+export type UpdateProductInput = z.TypeOf<typeof updateProductSchema>;
