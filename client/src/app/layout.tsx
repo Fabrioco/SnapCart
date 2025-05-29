4d3e00bcf4571879a97e0dc9b118a35798f152f9
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-29 19:57:48 -0300

    feat(layout): melhorei o metadata

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -7,6 +7,25 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Snap Cart",
   description: "Snap Cart, o seu carrinho de compras online",
+  icons: {
+    icon: "favicon.ico",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  openGraph: {
+    title: "Snap Cart",
+    description: "Snap Cart, o seu carrinho de compras online",
+    siteName: "Snap Cart",
+    locale: "pt-BR",
+    type: "website",
+  },
+  robots:{
+    index: true,
+    follow: true
+  }
 };
 
 export default function RootLayout({
