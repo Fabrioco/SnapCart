398546beb6a9a3845c90f245380a3604315bb723
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-29 20:42:12 -0300

    feat(loginPage): criei a estruturação inicial

---
+++ client/src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/ui/auth/login/form";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Bem vindo de volta!</h1>
+      <LoginForm />
+    </main>
+  );
+}
