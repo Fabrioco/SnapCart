7d9809d5be3578ec6eee3ecff5f243751abb2d48
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-29 21:02:30 -0300

    feat(loginPage): criei a estilização da pagina

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "@/ui/auth/login/form";
 
 export default function LoginPage() {
   return (
-    <main>
-      <h1>Bem vindo de volta!</h1>
+    <main className="flex flex-col gap-4 w-full h-full items-center justify-center">
+      <h1 className="text-2xl font-semibold">Bem vindo de volta!</h1>
       <LoginForm />
     </main>
   );
