477851e7e11c9ddef41babbe234627389ddb47ff
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-31 08:34:47 -0300

    feat(registerPage): criei a estilização final da pagina

--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -1,40 +1,14 @@
+import { RegisterAuthLink } from "@/ui/auth/register/authLinks";
+import { RegisterForm } from "@/ui/auth/register/form";
+
 export default function RegisterPage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-semibold w-fit">
         Seja bem-vindo! Cadastre-se
       </h1>
-
-      <form>
-        <div>
-          <label htmlFor="name">Nome</label>
-          <input type="text" id="name" name="name" />
-        </div>
-        <div>
-          <label htmlFor="numberPhone">Número de telefone</label>
-          <input type="text" id="numberPhone" name="numberPhone" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Senha</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit">Cadastrar</button>
-      </form>
-      <section>
-        <p>
-          Já possui uma conta?{" "}
-          <a
-            href="/auth/login"
-            className="hover:underline hover:text-orange-500"
-          >
-            Faça login
-          </a>
-        </p>
-      </section>
+      <RegisterForm />
+      <RegisterAuthLink />
     </main>
   );
 }
