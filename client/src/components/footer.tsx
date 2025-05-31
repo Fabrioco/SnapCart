08a7ec5f435af81be1496fc1e61402dab60d5afb
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-31 09:29:08 -0300

    feat(footer): criei o componente global de footer

---
+++ client/src/components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="flex justify-center items-center py-4">
+      <p className="text-sm text-gray-500">
+        &copy; {new Date().getFullYear()} SnapCart. All rights reserved.
+      </p>
+    </footer>
+  );
+}
