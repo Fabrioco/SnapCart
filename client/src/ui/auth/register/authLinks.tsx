9c16e54d513b99b7c2f7e0c1d65d350bb563708f
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-31 02:01:25 -0300

    feat(registerAuthLink): criei a componentização do section com link

---
+++ client/src/ui/auth/register/authLinks.tsx
@@ -0,0 +1,12 @@
+export function RegisterAuthLink() {
+  return (
+    <section>
+      <p>
+        Já possui uma conta?{" "}
+        <a href="/auth/login" className="hover:underline hover:text-orange-500">
+          Faça login
+        </a>
+      </p>
+    </section>
+  );
+}
