1b3ced86b7f214e58399c483cfceefdf1cd9c950
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-31 08:50:37 -0300

    fix(registerAuthLink): corrigi o tamanho do texto

--- client/src/ui/auth/register/authLinks.tsx
+++ client/src/ui/auth/register/authLinks.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function RegisterAuthLink() {
   return (
     <section className="flex flex-col gap-2 bg-white text-black p-4 rounded-md border border-solid border-gray-500 shadow-lg w-11/12 lg:w-1/3 mt-4">
-      <p>
+      <p className="text-sm">
         Já possui uma conta?{" "}
         <Link
           href="/auth/login"
