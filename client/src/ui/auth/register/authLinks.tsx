f43fe1f579c1587aea2d217af7711a30686da274
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-31 08:32:57 -0300

    feat(registerAuthLink): criei a estilização final do registerAuthLink

--- client/src/ui/auth/register/authLinks.tsx
+++ client/src/ui/auth/register/authLinks.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 export function RegisterAuthLink() {
   return (
-    <section>
+    <section className="flex flex-col gap-2 bg-white text-black p-4 rounded-md border border-solid border-gray-500 shadow-lg w-11/12 lg:w-1/3 mt-4">
       <p>
         Já possui uma conta?{" "}
-        <a href="/auth/login" className="hover:underline hover:text-orange-500">
+        <Link
+          href="/auth/login"
+          className="hover:underline hover:text-orange-500"
+        >
           Faça login
-        </a>
+        </Link>
       </p>
     </section>
   );
