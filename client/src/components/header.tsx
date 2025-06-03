d6130c8c854a1daa56948634690d86524cb83e5b
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-02 23:50:33 -0300

    fix(header-component): consertei a responsividade no mobile

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,11 +1,16 @@
 import { NavHeader } from "@/ui/header/nav";
 import ThemeToggle from "../ui/header/themeToggle";
 import React from "react";
+import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="w-full flex justify-between items-center px-4 py-2 border-b border-gray-300 shadow-md">
-      <h1 className="text-2xl font-semibold text-orange-500">SnapCart</h1>
+    <header className="w-full flex flex-col sm:flex-row gap-4 justify-between items-center px-4 py-2 border-b border-gray-300 shadow-md">
+      <h1 className="text-2xl font-semibold text-orange-500">
+        <Link href="/">
+          <p>SnapCart</p>
+        </Link>
+      </h1>
       <NavHeader />
       <ThemeToggle />
     </header>
