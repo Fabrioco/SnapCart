606f387b52ef100c8c174b328479736fe5376718
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-03 14:51:43 -0300

    feat(homePage): criei a estruturação da pagina

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main>
+      <section>
+        <h1>Bem-vindo(a) ao SnapCart</h1>
+        <p>A loja online que revoluciona a forma como você compra</p>
+        <button>Comece a comprar</button>
+      </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <section>
+        <h2>Sobre Nós</h2>
+        <p>
+          A loja SnapCart é uma empresa familiar que nasceu em 2010 em uma
+          pequena cidade do interior de São Paulo. Começou como uma pequena loja
+          de conveniência que vendia produtos de limpeza e higiene pessoal. Com
+          o passar do tempo, o pai e a mãe, os fundadores, perceberam que havia
+          uma grande demanda por produtos de qualidade em sua região e decidiram
+          ampliar o tipo de produtos que vendiam. Anos se passaram e a loja se
+          tornou uma loja de departamento, onde os clientes podiam encontrar
+          produtos de qualidade em uma grade variedade de categorias.
+        </p>
+        <p>
+          Em 2015, os filhos dos fundadores, que haviam crescido ajudando a
+          gerenciar a loja, decidiram criar um site para vender os produtos
+          online. Com isso, a loja SnapCart pode expandir seus negócios para
+          todo o país, e hoje em dia, é uma das principais lojas online do
+          Brasil.
+        </p>
+      </section>
+
+      <section>
+        <h2>Objetivos</h2>
+        <p>
+          Nossa missão é oferecer aos nossos clientes uma experiência de compra
+          online de alta qualidade, com preços competitivos e uma variedade de
+          produtos que atenda às necessidades de todos.
+        </p>
+        <p>
+          Nossa visão é ser a principal loja online do Brasil, reconhecida pela
+          qualidade dos nossos produtos e pelo excelente atendimento aos
+          clientes.
+        </p>
+      </section>
+
+      <section>
+        <h2>Produtos</h2>
+        <p>
+          Nós oferecemos uma grande variedade de produtos em diferentes
+          categorias, como:
+        </p>
+        <ul>
+          <li>Produtos de limpeza e higiene pessoal</li>
+          <li>Produtos de alimentação e bebida</li>
+          <li>Produtos de beleza e cuidado pessoal</li>
+          <li>Produtos de casa e decoração</li>
+          <li>Produtos de esporte e lazer</li>
+          <li>Produtos de informática e tecnologia</li>
+        </ul>
+      </section>
+    </main>
   );
 }
