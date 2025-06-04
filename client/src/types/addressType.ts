b3bffb8e955d0454e2e5e13137486564f5997822
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-04 15:42:24 -0300

    feat(addressType): criei a tipagem do address

---
+++ client/src/types/addressType.ts
@@ -0,0 +1,11 @@
+export type AddressType = {
+  id: number;
+  cep: string;
+  street: string;
+  number: number;
+  city: string;
+  state: string;
+  country: string;
+  type: string;
+  userId: number;
+};
