a503fc6bb8f50b5fbef18939c4072f3aa70be720
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-04 19:58:36 -0300

    feat(orderSection): criei a section de order, só mostra o id por enquanto

---
+++ client/src/ui/settings/orderSection.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { OrderType } from "@/types/orderType";
+import { useEffect, useState } from "react";
+
+export function OrderSection() {
+  const [orders, setOrders] = useState<OrderType[]>([]);
+  const [error, setError] = useState<string>("");
+  const fetchOrder = async () => {
+    const response = await fetch("http://localhost:5000/api/orders", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (data.message === "Nenhum pedido encontrado para o usuário") {
+      setError(data.message);
+      return;
+    }
+
+    setOrders(data);
+  };
+
+  useEffect(() => {
+    fetchOrder();
+  }, []);
+
+  return (
+    <section>
+      <h2>Pedidos</h2>
+      <ul>
+        {orders.length > 0 &&
+          orders.map((order) => (
+            <li key={order.id}>
+              <p>{order.id}</p>
+              <p>{order.orderStatus}</p>
+            </li>
+          ))}
+        {error && <p>{error}</p>}
+      </ul>
+    </section>
+  );
+}
