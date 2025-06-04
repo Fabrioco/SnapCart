aa59f13db89a3a4b921351f0346dae7ff6049cbd
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-03 22:34:28 -0300

    fix(userRoute): corrigi a url da api para não receber o id do usuário

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -15,7 +15,7 @@ const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", addUser);
-router.get("/users/:id", authMiddleware, getUser);
+router.get("/user", authMiddleware, getUser);
 router.patch("/users/:id", authMiddleware, updateUser);
 router.post("/users/login", login);
 router.post("/users/logout", authMiddleware, logout);
