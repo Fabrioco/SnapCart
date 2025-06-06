98a7557519978a60433beaf36b8714c731243284
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-06 01:35:57 -0300

    cleanup(settings-page): removi o 'use client'

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -1,21 +1,16 @@
-"use client";
-import { useCheckAuth } from "@/hooks/useCheckAuth";
 import ProtectedRoute from "@/routes/protectedRoute";
 import { AddressSection } from "@/ui/settings/addressSection";
 import { LogoutSection } from "@/ui/settings/logoutSection";
+import { NameSection } from "@/ui/settings/nameSection";
 import { OrderSection } from "@/ui/settings/orderSection";
 import { ProfileSection } from "@/ui/settings/profileSection";
 import React from "react";
 
 export default function SettingsPage() {
-  const { user, loading } = useCheckAuth();
-
   return (
     <ProtectedRoute>
       <main>
-        <section>
-          <h1>Olá, {!loading && user.name.split(" ")[0]}! Seja bem-vindo</h1>
-        </section>
+        <NameSection />
 
         <ProfileSection />
 
