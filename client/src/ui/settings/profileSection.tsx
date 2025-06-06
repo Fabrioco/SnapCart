3b43b2f5285634a9d09abdfad021870ee8c4381e
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-06 07:11:26 -0300

    feat(style-profile-section-settings-page): criei a estiização final da section

--- client/src/ui/settings/profileSection.tsx
+++ client/src/ui/settings/profileSection.tsx
@@ -14,6 +14,7 @@ export function ProfileSection() {
     setPassword,
     formatPhone,
     handleUpdateUser,
+    loading,
   } = useUserUpdateStore();
 
   const handleChangeNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,9 +25,12 @@ export function ProfileSection() {
   const { user } = useCheckAuth();
 
   return (
-    <section>
-      <h2>Perfil</h2>
-      <form onSubmit={(e) => handleUpdateUser(e, user.id)}>
+    <section className="flex flex-col justify-center items-start gap-4">
+      <h2 className="text-2xl font-semibold">Perfil</h2>
+      <form
+        onSubmit={(e) => handleUpdateUser(e, user.id)}
+        className="flex flex-col gap-2 w-full lg:grid lg:grid-cols-2 lg:gap-4 lg:w-1/2"
+      >
         <input
           type="text"
           placeholder="Nome Completo"
@@ -34,15 +38,17 @@ export function ProfileSection() {
           onChange={(e) => setName(e.target.value)}
           name="name"
           id="name"
+          className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-500"
         />
         <input
           type="text"
-          placeholder="(xx) xxxxx-xxxx (apenas números)"
+          placeholder="(XX) XXXXX-XXXX (apenas números)"
           id="number"
           name="number"
           value={numberPhone}
           onChange={handleChangeNumber}
           max={15}
+          className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-500"
         />
         <input
           type="email"
@@ -51,6 +57,7 @@ export function ProfileSection() {
           onChange={(e) => setEmail(e.target.value)}
           name="email"
           id="email"
+          className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-500"
         />
         <input
           type="password"
@@ -59,8 +66,15 @@ export function ProfileSection() {
           placeholder="Nova senha"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-500"
         />
-        <button type="submit">Salvar</button>
+        <button
+          type="submit"
+          className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600 disabled:bg-gray-400 disabled:cursor-not-allowed lg:col-span-2 lg:w-fit lg:mr-auto"
+          disabled={loading}
+        >
+          {loading ? "Atualizando..." : "Atualizar"}
+        </button>
       </form>
     </section>
   );
