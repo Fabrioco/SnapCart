a88f3fede4a656b446702b0715e899270dad31f2
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-06 01:26:01 -0300

    feat(settings-page): adicionei as section de orders e logout

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -2,6 +2,8 @@
 import { useCheckAuth } from "@/hooks/useCheckAuth";
 import ProtectedRoute from "@/routes/protectedRoute";
 import { AddressSection } from "@/ui/settings/addressSection";
+import { LogoutSection } from "@/ui/settings/logoutSection";
+import { OrderSection } from "@/ui/settings/orderSection";
 import { ProfileSection } from "@/ui/settings/profileSection";
 import React from "react";
 
@@ -19,29 +21,9 @@ export default function SettingsPage() {
 
         <AddressSection />
 
-        <section>
-          <h2>Pedidos</h2>
-          <ul>
-            <li>
-              <h3>Pedido #12345</h3>
-              <p>Status: Em Andamento</p>
-              <ul>
-                <li>Produto 1: Quantidade 2</li>
-                <li>Produto 2: Quantidade 1</li>
-              </ul>
-              <p>Data do Pedido: 10/10/2023</p>
-              <p>Total: R$ 150,00</p>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>Sair da Conta</h2>
-          <ul>
-            <li>
-              <a href={`/logout`}>Sair</a>
-            </li>
-          </ul>
-        </section>
+        <OrderSection />
+
+        <LogoutSection />
       </main>
     </ProtectedRoute>
   );
