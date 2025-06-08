abe1ba1d41964bc7c1ae3f9d15c69021c1b34d39
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-08 16:52:40 -0300

    feat(product-remove-form-product-dashboard-page): criei o form de remover um produto pelo id

---
+++ client/src/ui/admin/dashboard/products/forms/removeUpdateForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { ProductType } from "@/types/productType";
+import { useEffect, useState } from "react";
+
+export default function RemoveProductForm() {
+  const [productId, setProductId] = useState("");
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!productId) {
+      alert("Preencha o campo ID do produto");
+      return;
+    }
+
+    try {
+      const res = await fetch(
+        `http://localhost:5000/api/products/${productId}`,
+        {
+          method: "DELETE",
+          credentials: "include",
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("Erro ao remover produto");
+      }
+
+      alert("Produto removido com sucesso");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchProducts = async () => {
+    const res = await fetch("http://localhost:5000/api/products", {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (!res.ok) {
+      throw new Error("Erro ao buscar produtos");
+    }
+    const data = await res.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <form
+      aria-labelledby="remove-product-form-title"
+      onSubmit={handleFormSubmit}
+    >
+      <fieldset>
+        <legend id="remove-product-form-title">Remover Produto</legend>
+        <label htmlFor="removeProductId">ID do Produto</label>
+        <select
+          name="removeProductId"
+          id="removeProductId"
+          value={productId}
+          onChange={(event) => setProductId(event.target.value)}
+        >
+          <option value="">Selecione um produto</option>
+          {products.map((product) => (
+            <option key={product.id} value={product.id}>
+              {product.name}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Remover Produto</button>
+      </fieldset>
+    </form>
+  );
+}
