1e1ad27dc9c57fe7dd96aa24ac332b58e8a8a53f
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-10 23:44:08 -0300

    feat(style-product-section-admin-page): fiz a estilização da section de produtos

--- client/src/app/admin/dashboard/products/page.tsx
+++ client/src/app/admin/dashboard/products/page.tsx
@@ -1,11 +1,19 @@
 import { AddProductForm } from "@/ui/admin/dashboard/products/forms/addProductForm/page";
-import RemoveProductForm from "@/ui/admin/dashboard/products/forms/removeUpdateForm";
+import RemoveProductForm from "@/ui/admin/dashboard/products/forms/removeProductForm/page";
 import UpdateProductForm from "@/ui/admin/dashboard/products/forms/updateProductForm/page";
 
 export default function DashboardProductsPage() {
   return (
-    <section aria-labelledby="product-management-title">
-      <h1 id="product-management-title">Gerenciamento de Produtos</h1>
+    <section
+      aria-labelledby="product-management-title"
+      className="flex flex-col gap-4 w-full h-full items-center justify-center"
+    >
+      <h1
+        id="product-management-title"
+        className="text-4xl font-bold text-orange-500"
+      >
+        Gerenciamento de Produtos
+      </h1>
 
       <AddProductForm />
 
