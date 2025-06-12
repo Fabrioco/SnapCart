2153c01d2f3a80ff4ccc34ea902d1eadbf6f29b6
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-12 14:34:18 -0300

    feat(order-with-user-and-address-type): criei a tipagem de pedido que tem a tipagem do usuario e do endereço

--- client/src/types/orderType.ts
+++ client/src/types/orderType.ts
@@ -1,9 +1,26 @@
+import { AddressType } from "./addressType";
+import { UserType } from "./userType";
+
 export type OrderType = {
-    id: number;
-    userId: number;
-    addressId: number;
-    paymentId: string;  
-    total: number;
-    orderStatus: string;
-    createdAt: string;
+  id: number;
+  userId: number;
+  addressId: number;
+  paymentId: string;
+  total: number;
+  orderStatus: string;
+  createdAt: string;
+};
+
+export type OrderUserAddress = OrderType & {
+  user: UserType;
+  address: AddressType;
+  items: [
+    {
+      id: number;
+      productId: number;
+      quantity: number;
+      price: number;
+      orderId: number;
+    }
+  ];
 };
