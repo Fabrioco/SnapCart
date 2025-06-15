16a12eb1b5719b5c6ad598888bd5e0205a3fd272
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-14 22:52:53 -0300

    feat(style-order-page-admin-dashboard-page): fiz a estilização do forms e do titulo

--- client/src/app/admin/dashboard/orders/page.tsx
+++ client/src/app/admin/dashboard/orders/page.tsx
@@ -4,7 +4,6 @@ import { FormSearch } from "@/ui/admin/dashboard/orders/formSearch";
 import { OrderList } from "@/ui/admin/dashboard/orders/orderList";
 import React from "react";
 
-
 export default function DashboardOrderPage() {
   const [filteredOrders, setFilteredOrders] = React.useState<
     OrderUserAddress[]
@@ -12,8 +11,13 @@ export default function DashboardOrderPage() {
   const [allOrders, setAllOrders] = React.useState<OrderUserAddress[]>([]);
 
   return (
-    <section aria-labelledby="order-management-title">
-      <h1 id="order-management-title">Gerenciamento de Pedidos</h1>
+    <section
+      aria-labelledby="order-management-title"
+      className="flex flex-col gap-4 w-full h-full items-center justify-center"
+    >
+      <h1 id="order-management-title" className="text-4xl font-bold">
+        Gerenciamento de Pedidos
+      </h1>
 
       <FormSearch allOrders={allOrders} setFilteredOrders={setFilteredOrders} />
 
