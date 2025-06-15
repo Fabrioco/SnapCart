5a68afbb1b22ebd5b712f35e349132bdeaec98f4
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-14 22:53:42 -0300

    feat(form-search-component-admin-dashboard-page): fiz a estilização do form de busca de pedidos

--- client/src/ui/admin/dashboard/orders/formSearch.tsx
+++ client/src/ui/admin/dashboard/orders/formSearch.tsx
@@ -12,7 +12,8 @@ export function FormSearch({
 }) {
   const [searchTerm, setSearchTerm] = React.useState<string>("");
 
-  const handleFilterOrders = () => {
+  const handleFilterOrders = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const filtered = allOrders.filter((order) =>
       order.user.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
@@ -20,14 +21,23 @@ export function FormSearch({
   };
 
   return (
-    <form onSubmit={handleFilterOrders}>
-      <input
-        type="text"
-        placeholder="Buscar por nome"
-        onChange={(e) => setSearchTerm(e.target.value)}
-        value={searchTerm}
-      />
-      <button type="submit">Buscar</button>
+    <form onSubmit={handleFilterOrders} className="w-11/12 mx-auto flex gap-2">
+      <div className="flex w-full border border-solid border-gray-500 px-2 py-1 rounded focus-within:outline-none focus-within:border-orange-500 focus-within:ring-2 focus-within:ring-orange-500 items-center justify-between">
+        <input
+          type="text"
+          placeholder="Buscar por nome"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          className="w-full focus:outline-none"
+        />
+        <span className="cursor-pointer">Limpar</span>
+      </div>
+      <button
+        type="submit"
+        className="bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 rounded"
+      >
+        Buscar
+      </button>
     </form>
   );
 }
