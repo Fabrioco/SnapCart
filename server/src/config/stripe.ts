77ead98ffdcc062a3e31d8d3a1066edc3ced62dc
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-16 10:11:10 -0300

    fix(prisma): correção do prisma url

--- server/src/config/stripe.ts
+++ server/src/config/stripe.ts
@@ -5,5 +5,5 @@ dotenv.config();
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2025-05-28.basil",
 });
