7ed5721b349ac05bb5ca2c06a21f384929804368
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-15 23:15:19 -0300

    fix(cart-page): arrumei o html se caso não tiver produtos no carrinho

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -3,6 +3,8 @@ import { AddressType } from "@/types/addressType";
 import { CartItemType, CartItemTypeWithProduct } from "@/types/cartItemType";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import ProtectedRoute from "@/routes/protectedRoute";
 
 export const fetchCartItems = async () => {
   try {
@@ -72,11 +74,15 @@ export default function CartPage() {
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const [addresses, setAddresses] = useState<AddressType[]>([]);
   const [selectedAddress, setSelectedAddress] = useState<number | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     fetchCartItems().then((data) => {
       setCartItems(data as CartItemTypeWithProduct[]);
-      if (!data) return;
+      if (!data) {
+        router.push("/products");
+        return;
+      }
       const totalPrice = data.reduce(
         (total, item) => total + item.product.price * item.quantity,
         0
@@ -87,7 +93,7 @@ export default function CartPage() {
     fetchAddress().then((data) => {
       setAddresses(data);
     });
-  }, []);
+  }, [router]);
 
   const finalizePurchase = async () => {
     if (selectedAddress === null) {
@@ -113,73 +119,101 @@ export default function CartPage() {
     window.location.href = data.url;
   };
 
-  return (
-    <main className="max-w-4xl mx-auto px-4 py-8 text-gray-800">
-      <h1 className="text-2xl sm:text-3xl font-bold text-center mb-8">
-        Carrinho
-      </h1>
-      <section className="space-y-4">
-        {cartItems.map((item) => (
-          <div
-            key={item.id}
-            className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col sm:flex-row sm:items-center sm:gap-8 p-4"
+  if (!cartItems.length) {
+    return (
+      <ProtectedRoute>
+        <main className="max-w-4xl mx-auto px-4 py-8 text-gray-800">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-8">
+            Carrinho vazio
+          </h1>
+          <p className="text-lg text-center">Nenhum produto no carrinho.</p>
+          <button
+            onClick={() => router.push("/products")}
+            className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600 w-full "
           >
-            <Image
-              src={
-                typeof item.product.image === "string" ? item.product.image : ""
-              }
-              alt={item.product.name}
-              width={200}
-              height={200}
-              className="w-full sm:w-64 h-48 object-cover rounded-md"
-            />
-            <div className="flex-1 mt-4 sm:mt-0">
-              <h2 className="text-lg sm:text-xl font-semibold">
-                Produto:{" "}
-                <span className="text-gray-600">{item.product.name}</span>
-              </h2>
-              <p className="text-md sm:text-lg font-medium mt-2">
-                Quantidade: <span className="font-bold">{item.quantity}</span>
-              </p>
-            </div>
-          </div>
-        ))}
-      </section>
-      <section className="mt-6 space-y-4">
-        <p className="font-medium">Escolha um endereço de entrega:</p>
-        <div className="space-y-2">
-          {addresses.map((address) => (
-            <div key={address.id} className="p-4 border rounded-md">
-              <p className="font-medium">{address.street}, {address.number}</p>
-              <p>{address.city}, {address.state}</p>
-              <p>{address.cep}</p>
-              <input
-                type="radio"
-                name="address"
-                value={address.id}
-                onChange={(e) => setSelectedAddress(Number(e.target.value))}
-                className="mt-2"
+            Ver produtos
+          </button>
+        </main>
+      </ProtectedRoute>
+    );
+  }
+
+  return (
+    <ProtectedRoute>
+      <main className="max-w-4xl mx-auto px-4 py-8 text-gray-800">
+        <h1 className="text-2xl sm:text-3xl font-bold text-center mb-8">
+          Carrinho
+        </h1>
+        <section className="space-y-4">
+          {cartItems.map((item) => (
+            <div
+              key={item.id}
+              className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col sm:flex-row sm:items-center sm:gap-8 p-4"
+            >
+              <Image
+                src={
+                  typeof item.product.image === "string"
+                    ? item.product.image
+                    : ""
+                }
+                alt={item.product.name}
+                width={200}
+                height={200}
+                className="w-full sm:w-64 h-48 object-cover rounded-md"
               />
+              <div className="flex-1 mt-4 sm:mt-0">
+                <h2 className="text-lg sm:text-xl font-semibold">
+                  Produto:{" "}
+                  <span className="text-gray-600">{item.product.name}</span>
+                </h2>
+                <p className="text-md sm:text-lg font-medium mt-2">
+                  Quantidade: <span className="font-bold">{item.quantity}</span>
+                </p>
+              </div>
             </div>
           ))}
-        </div>
-      </section>
-      <section className="mt-6">
-        <p className="font-medium">Infelizmente nossa loja apenas aceita pagamento com cartão.</p>
-      </section>
-      <section className="mt-6">
-        <button
-          className="w-full py-2 bg-orange-500 text-white rounded hover:bg-orange-600 transition-all"
-          onClick={finalizePurchase}
-        >
-          Finalizar Compra -{" "}
-          {totalPrice.toLocaleString("pt-br", {
-            style: "currency",
-            currency: "BRL",
-          })}
-        </button>
-      </section>
-    </main>
+        </section>
+        <section className="mt-6 space-y-4">
+          <p className="font-medium">Escolha um endereço de entrega:</p>
+          <div className="space-y-2">
+            {addresses.map((address) => (
+              <div key={address.id} className="p-4 border rounded-md">
+                <p className="font-medium">
+                  {address.street}, {address.number}
+                </p>
+                <p>
+                  {address.city}, {address.state}
+                </p>
+                <p>{address.cep}</p>
+                <input
+                  type="radio"
+                  name="address"
+                  value={address.id}
+                  onChange={(e) => setSelectedAddress(Number(e.target.value))}
+                  className="mt-2"
+                />
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className="mt-6">
+          <p className="font-medium">
+            Infelizmente nossa loja apenas aceita pagamento com cartão.
+          </p>
+        </section>
+        <section className="mt-6">
+          <button
+            className="w-full py-2 bg-orange-500 text-white rounded hover:bg-orange-600 transition-all"
+            onClick={finalizePurchase}
+          >
+            Finalizar Compra -{" "}
+            {totalPrice.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </button>
+        </section>
+      </main>
+    </ProtectedRoute>
   );
 }
-
