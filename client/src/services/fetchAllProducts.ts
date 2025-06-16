6c6211f712a6cedf94a4f528c807d1575f9a847e
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-06-16 14:26:39 -0300

    refactor(fetch-products-cart-page): coloquei o hook fetchAllProducts em um arquivo separado

---
+++ client/src/services/fetchAllProducts.ts
@@ -0,0 +1,19 @@
+import { ProductType } from "@/types/productType";
+
+export const fetchAllProducts = async (): Promise<ProductType[]> => {
+  const res = await fetch("http://localhost:5000/api/products", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    console.error(await res.json());
+    throw new Error("Erro ao buscar produtos");
+  }
+
+  const data = await res.json();
+  return data;
+};
